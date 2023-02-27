c929f3b26321ba94a1a47b6999b3c741c000a780
Marcel Blijleven <marcelblijleven@gmail.com>
2023-02-27 22:51:09 +0100

    chore: format file

--- tests/fittie/fitfile/test_data_message.py
+++ tests/fittie/fitfile/test_data_message.py
@@ -10,14 +10,11 @@ def test_decode_data_message(record_1_definition_message):
         is_definition_message=False,
         is_developer_data=False,
         is_compressed_timestamp_message=False,
-        local_message_type=0
+        local_message_type=0,
     )
-    data = b'\x04\x0f\x00\x16\x00\xd2\x04\x00\x00(\xc6\n%'
+    data = b"\x04\x0f\x00\x16\x00\xd2\x04\x00\x00(\xc6\n%"
     data_message = decode_data_message(
-        header,
-        record_1_definition_message,
-        developer_data={},
-        data=BytesIO(data)
+        header, record_1_definition_message, developer_data={}, data=BytesIO(data)
     )
     assert data_message.fields == {
         "type": 4,
@@ -34,25 +31,22 @@ def test_decode_data_message_with_developer_fields(record_5_definition_message):
         is_definition_message=False,
         is_developer_data=False,
         is_compressed_timestamp_message=False,
-        local_message_type=0
+        local_message_type=0,
     )
     data = (
-        b'\x00\x00\x01'
-        b'doughnuts earned\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-        b'doughnuts\x00\x00\x00\x00\x00\x00\x00'
+        b"\x00\x00\x01"
+        b"doughnuts earned\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        b"doughnuts\x00\x00\x00\x00\x00\x00\x00"
     )
     data_message = decode_data_message(
-        header,
-        record_5_definition_message,
-        developer_data={},
-        data=BytesIO(data)
+        header, record_5_definition_message, developer_data={}, data=BytesIO(data)
     )
     assert data_message.fields == {
         "developer_data_index": 0,
         "fit_base_type_id": 1,
         "field_definition_number": 0,
         "field_name": "doughnuts earned",
-        "units": "doughnuts"
+        "units": "doughnuts",
     }
